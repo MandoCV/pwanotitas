e4147ee21f8a015a19bdf69e9745d21c6a464bf7
Mando CV <210124@utxicotepec.edu.mx>
2024-11-28 16:59:04 -0600

    fdsfs

--- src/components/Notes.vue
+++ src/components/Notes.vue
@@ -2,13 +2,32 @@
 <div class="hello">
 <h1>{{msg}}</h1>
 </div>
+
+<div class="notes">
+    <ul>
+        <li v-for="(page, index) of pages" class="page">
+            <div> {{page.title }}</div>
+            <div> {{page.content }}</div>
+
+        </li>
+        <li class="new-note">
+            <v-btn fab dark color="indigo" @click="newNote()">
+                <v-icon dark>add</v-icon>
+            </v-btn>
+        </li>
+
+    </ul>
+</div>
 </template>
 
 <script>
 export default{
     name:'Notes',
-    props:{
-        msg: String
+    props:['pages'],
+    methods: {
+        newNote(){
+            this.$emit('new-note')
+        }
     }
 }
 </script>
